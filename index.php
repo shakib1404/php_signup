73a6ae24567b8dbfbc8eeaf7bcea24bd730ec33d
shakib1404 <bsse1404@iit.du.ac.bd>
2025-01-23 19:45:22 +0600

    login

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login and register</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <H2>SIR PLEASE ENTER</H2>
+    <nav>
+    <a href="">HOME</a>
+    <a href="">BLOCK</a>
+    <a href="">CONTACT</a>
+    <a href="">ABOUT</a>
+
+    </nav>
+
+    <div class="sign-in-up">
+        <button type="button">LOGIN</button>
+        <button type="button">REGISTER</button>
+
+
+    </div>
+
+
+    </header>
+    
+</body>
+</html>
